d9e24cca56c9364172cd843604e407549887074e
Edouard KLEIN <edouard@halvskugga.lan>
2013-03-19 08:29:05 +0100

    Exp13 : CSI works on the Highway

--- Exp13.py
+++ Exp13.py
@@ -24,11 +24,13 @@ ACTION_SPACE = range(0,5)
 
 Highway = MDP(P,R)
 mPi_E, V_E, Pi_E = Highway.optimal_policy()
+Highway.evaluate(mPi_E)
 
 # <codecell>
 
 rho = lambda : int(rand()*729) #uniform distribtion over S
-D_E = array(Highway.D_func(Pi_E, 1, 100,  rho))
+l_D_E = [array(Highway.D_func(Pi_E, 1, 7,  rho)) for i in range(0,7)]
+D_E = vstack(l_D_E)
 
 # <codecell>
 
@@ -73,7 +75,7 @@ class GradientDescent(object):
 
 class StructuredClassifier(GradientDescent):
     sign=-1.
-    Threshold=0.1 #Sensible default
+    Threshold=0.01 #Sensible default
     T=100 #Sensible default
     phi=None
     label_set=None
@@ -123,13 +125,19 @@ for i,j in zip(range(0,len(D_E)),D_E[:,0]):
     Psi[i,j]=1.
 clf = StructuredClassifier(Psi, D_E[:,1], 5)
 theta_C = clf.run()
+mPi_C = Highway.Q2Pi(theta_C)
+
 def single_phi(sa):
     answer = zeros((3645,1))
     answer[sa[0] + 729*sa[1]] = 1.
     return answer
 phi = non_scalar_vectorize(single_phi, (2,), (3645,1))
 q = lambda sa: squeeze(dot(theta_C.transpose(),phi(sa)))
-pi_c = greedy_policy(theta_C,phi,ACTION_SPACE, s_dim=1)
+#pi_c = greedy_policy(theta_C,phi,ACTION_SPACE, s_dim=1)
+single_pi_c = lambda s : Highway.control(s, mPi_C)
+pi_c = vectorize(single_pi_c)
+Highway.evaluate(mPi_C)
+#theta_C.min(),theta_C.max(),theta_C.mean(),theta_C.var()
 
 # <codecell>
 
@@ -143,23 +151,24 @@ a_dash = pi_c(s_dash).reshape(column_shape)
 sa_dash = hstack([s_dash,a_dash])
 hat_r = (q(sa)-Gamma*q(sa_dash)).reshape(column_shape)
 r_min = min(hat_r)-1.*ones(column_shape)
+hat_r.max(),hat_r.min(),hat_r.mean(),hat_r.var()
+
+# <codecell>
 
 ##Avec l'heuristique : 
-regression_input_matrices = [hstack([s,action*ones(column_shape)]) for action in ACTION_SPACE] 
-def add_output_column( reg_mat ):
-    actions = reg_mat[:,-1].reshape(column_shape)
-    hat_r_bool_table = array(actions==a)
+thetas = zeros((5,729))
+for action in ACTION_SPACE:
+    X = Psi
+    hat_r_bool_table = array(a==action)
     r_min_bool_table = array(hat_r_bool_table==False) #"not hat_r_bool_table" does not work as I expected
-    output_column = hat_r_bool_table*hat_r+r_min_bool_table*r_min
-    return hstack([reg_mat,output_column])
-regression_matrix = vstack(map(add_output_column,regression_input_matrices))
-#Régression
-from sklearn.svm import SVR
-y = regression_matrix[:,-1]
-X = regression_matrix[:,:-1]
-reg = SVR(C=1.0, epsilon=0.2)#, gamma=1/(2*pow(0.03,2)))
-reg.fit(X, y)
-fCSI_reward = lambda sa:reg.predict(sa)[0]
+    Y =  hat_r_bool_table*hat_r + r_min_bool_table*r_min
+    #if sum(hat_r_bool_table) == 0:
+    #    thetas[action] = zeros(729)
+    #else:
+    thetas[action] = squeeze( dot(dot(inv(dot(X.transpose(),X)+0.1*identity(X.shape[1])),X.transpose()),Y) )
+theta_CSI = thetas.reshape(3645)
+fCSI_reward = lambda sa:dot(theta_CSI.transpose(),phi(sa))
+theta_CSI.min(),theta_CSI.max(),theta_CSI.mean(),theta_CSI.var()
 
 # <codecell>
 
@@ -183,22 +192,27 @@ def s_index( state ):
     return index
 
 def sa_index( state, action ):
-    index = s_index(state) + a*3*9*9*3
+    index = s_index(state) + action*3*9*9*3
     return index
 
 reward_CSI=zeros((3645,1))
 for state in S:
     for action in ACTION_SPACE:
         index = sa_index(state, action)
-        reward_CSI[index] = fCSI_reward(array([state,action]))
+        reward_CSI[index] = fCSI_reward(array([s_index(state),action]))
+Highway2 = MDP(P,reward_CSI)
+mPi_A, V_A, Pi_A = Highway2.optimal_policy()
+Highway.evaluate(mPi_A)
+#reward_CSI.min(),reward_CSI.max(),reward_CSI.mean(),reward_CSI.var()
 
 # <codecell>
 
-Highway2 = MDP(P,reward_CSI)
-mPi_A, V_A, Pi_A = Highway2.optimal_policy()
-true_V_A = linalg.solve( identity( 729 ) - 0.9*dot(mPi_A,P), dot( mPi_A, R) )
+
 Highway3 = MDP(P,rand(3645,1))
 mPi_R, V_R, Pi_R = Highway3.optimal_policy()
-true_V_R = linalg.solve( identity( 729 ) - 0.9*dot(mPi_R,P), dot( mPi_R, R) )
-mean(true_V_A),mean(true_V_R),mean(V_E)
+Highway.evaluate(mPi_R)
+
+# <codecell>
+
+savetxt('RewardFaisantPlanterDP.mat',reward_CSI)
 
